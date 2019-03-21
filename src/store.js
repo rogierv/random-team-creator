a72d85fe78618ac5bfa27decf31f13a578732abe
Rogier Verkaik <rogierv@gmail.com>
2019-03-21 23:49:29 +0100

    implement redux-localstorage-simple

--- src/store.js
+++ src/store.js
@@ -1,11 +1,12 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import ReduxThunk from 'redux-thunk';
 import reducer from './reducers';
+import { save, load } from 'redux-localstorage-simple';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const enhancer = composeEnhancers(applyMiddleware(ReduxThunk));
+const enhancer = composeEnhancers(applyMiddleware(ReduxThunk, save({ states: ['groups'], namespace: 'redux' })));
 
-const store = createStore(reducer, enhancer);
+const store = createStore(reducer, load({ states: ['groups'], namespace: 'redux' }), enhancer);
 
 export default store;
